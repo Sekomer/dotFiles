10d2b070cc5c76170d499288a4014a1d20bd67cf
sekomer <a.serkanaksoz@gmail.com>
2021-01-17 18:09:50 +0300

    pip 2&3 combiner

--- pip_script.sh
+++ pip_script.sh
@@ -13,6 +13,7 @@ YL='\033[0;33m'
 for ((i = 0; i < $#; i++))
 {
     echo -e "${YL}Downloading ${args[$i]} for Python2${NC}"
+
     for ((j = 0; j < 3; j++))
     {
     	printf "\033[0;31m. \033[0m"
@@ -22,10 +23,12 @@ for ((i = 0; i < $#; i++))
     printf "\n\n"
  	
  	pip2 install ${args[$i]}
+
     sleep 1
+
     echo -e "${YL}\nDownloading ${args[$i]} for Python3${NC}"
  	
- 	for ((k = 0; k < 3; k++))
+	for ((k = 0; k < 3; k++))
     {
     	printf "\033[0;31m. \033[0m"
     	sleep .5
